5347bd0e6ad61ed367eda4e7e8b0a830c140f9a4
p7e4 <44750087+p7e4@users.noreply.github.com>
2021-12-10 22:43:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,17 +2,25 @@ apt-get update && apt-get upgrade -y
 
 apt-get install golang mongodb wget pigz jq -y
 
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn,https://gocenter.io,https://goproxy.io,direct
+
+service mongodb start
+
 wget -O fdns.json.gz https://opendata.rapid7.com/sonar.fdns_v2/2021-03-26-1616717346-fdns_a.json.gz
 wget -O rdns.json.gz https://opendata.rapid7.com/sonar.rdns_v2/2021-03-24-1616544312-rdns.json.gz
 
-pigz -dc fdns.json.gz | grep "cname\"" -v | jq -r '"}\"" + .value + "\":\"pi\",\"" + .name + "\":\"niamod\"{"' | tr '[:upper:]' '[:lower:]' | rev > tmp.json
-pigz -dc rdns.json.gz | jq -r '"}\"" + .name + "\":\"pi\",\"" + .value + "\":\"niamod\"{"' | tr '[:upper:]' '[:lower:]' | rev >> tmp.json
+pigz -dc fdns.json.gz | grep "cname\"" -v | jq -r '"}\"" + .value + "\":\"pi\",\"" + .name + "\":\"niamod\"{"' | tr '[:upper:]' '[:lower:]' | rev | pigz > tmp.json.gz
+pigz -dc rdns.json.gz | grep "cname\"" -v | jq -r '"}\"" + .name + "\":\"pi\",\"" + .value + "\":\"niamod\"{"' | tr '[:upper:]' '[:lower:]' | rev | pigz >> tmp.json.gz
 
-rm -f rdns.json.gz fdns.json.gz
+rm rdns.json.gz fdns.json.gz
 
-grep -E '^\{"domain":"[-a-z0-9.]+","ip":"([0-9]{1,3}\.){3}[0-9]{1,3}"}$' tmp.json | sort | uniq | mongoimport -d dns -c dns
+# speed up options for sort: --parallel=8 --buffer-size=30G
+pigz -dc tmp.json.gz | grep -E '^\{"domain":"[-a-z0-9.]+","ip":"([0-9]{1,3}\.){3}[0-9]{1,3}"}$' | sort -u | pigz > result.json.gz
 
-rm tmp.json
+pigz -dc result.json.gz | mongoimport -d dns -c dns
+
+rm tmp.json.gz result.json.gz
 
 mongo dns --eval "db.dns.createIndex({domain: 1});db.dns.createIndex({ip: 1})"
 
